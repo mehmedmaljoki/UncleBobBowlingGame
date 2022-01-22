0c734e555c2c98ae078d7b8d0988777b79323dcb
Mehmed Maljoki <mehmed.maljoki@porscheinformatik.com>
2022-01-22 15:25:32 +0100

    adding algorithm and the rest of tests

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+G
+UncleBobBowlingGame.iml,f\a\fa9769fe1641ec910e207a27f9464c1470c5dd6a
--- src/BowlingGame/BowlingTest.java
+++ src/BowlingGame/BowlingTest.java
@@ -1,11 +1,23 @@
 package BowlingGame;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BowlingTest {
 
     private Game g;
+
+    private void rollSpare() {
+        g.roll(5);
+        g.roll(5);
+    }
+
+    private void rollStrike() {
+        g.roll(10);
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -15,5 +27,46 @@ public class BowlingTest {
     @Test
     public void canRoll() throws Exception {
         g.roll(0);
+    }
+
+    @Test
+    public void gutterGame() throws Exception {
+        rollMany(20, 0);
+        assertEquals(0, g.score());
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) {
+            g.roll(pins);
+        }
+    }
+
+    @Test
+    public void allOnes() throws Exception {
+        rollMany(20, 1);
+        assertEquals(20, g.score());
+    }
+
+    @Test
+    public void oneSpare() throws Exception {
+        rollSpare();
+        g.roll(3);
+        rollMany(17, 0);
+        assertEquals(16, g.score());
+    }
+
+    @Test
+    public void oneStrike() throws Exception {
+        rollStrike();
+        g.roll(3);
+        g.roll(4);
+        rollMany(16, 0);
+        assertEquals(24, g.score());
+    }
+
+    @Test
+    public void perfectGame() throws Exception {
+        rollMany(12, 10);
+        assertEquals(300, g.score());
     }
 }
